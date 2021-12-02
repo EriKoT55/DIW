f28d8caa72e883786da89cb0a0686a361bdcfcf1
EriKoT55 <erik55@outlook.es>
2021-12-02 20:56:53 +0100

    2 dic 2021

--- ActividadAudio/javaScript.js
+++ ActividadAudio/javaScript.js
@@ -1,6 +1,24 @@
-var playStop= document.getElementById("player");
-var sonando=false;
 
 function play_parar(){
+    let playPause=document.getElementById("player");
+
+    if(playPause.paused){
+        playPause.play(); 
+        document.getElementById("play").src="img/pause.jpg";
+    }else{
+        playPause.pause();
+        document.getElementById("play").src="img/play.png";
+    }
+}   
+
+function stop(){
+    let stop=document.getElementById("player");
+
+    if(stop.paused){
+       
+    }else{
+        stop.pause();
+        stop.currentTime=0;
+    }
 
 }
