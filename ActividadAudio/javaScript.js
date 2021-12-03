021d89526fdad9ac3fcc6d76eb60e80532cb3300
EriKoT55 <erik55@outlook.es>
2021-12-03 19:15:49 +0100

    3 dic 2021

--- ActividadAudio/javaScript.js
+++ ActividadAudio/javaScript.js
@@ -2,12 +2,13 @@
 function play_parar(){
     let playPause=document.getElementById("player");
 
-    if(playPause.paused){
+    if(playPause.paused ){
         playPause.play(); 
         document.getElementById("play").src="img/pause.jpg";
     }else{
         playPause.pause();
         document.getElementById("play").src="img/play.png";
+
     }
 }   
 
@@ -17,18 +18,20 @@ function stop(){
     if(!stop.paused){
         stop.pause();
         stop.currentTime=0;
+        document.getElementById("play").src="img/play.png";
     }
-
 }
 
-function nombre_cancion(){
-    
+function nombre_canciones(){
+    let songs=["WOS_LLENO_DE_ZAFIROS.wav","XXXTENTACION.mp3","Voz.m4a","DUKI_Rockstar.ogg"];
+    let nombreCanciones=document.getElementById("nombreCancion");
+    nombreCanciones.textContent=songs[contador];
 }
 
 function next(){
-let songs=["WOS_LLENO_DE_ZAFIROS.wav","XXXTENTACION.mp3","Voz.m4a","DUKI_Rockstar.ogg"];
-let contador=0;
-let seguir=false;
+    let songs=["WOS_LLENO_DE_ZAFIROS.wav","XXXTENTACION.mp3","Voz.m4a","DUKI_Rockstar.ogg"];
+    let contador=0;
+    let seguir=false;
     contador++;
 
 
@@ -36,6 +39,6 @@ let seguir=false;
         contador=0;
     }
     songs.src="songs/"+songs[contador];
-    seguir ? songs.play() : "";
-
+    seguir ? songs.play():"";
+    nombre_canciones();
 }
